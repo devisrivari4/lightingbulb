ff7939051f3f22b0af263eb8c922c4210aa6534d
devisrivari4 <devisrivari4@gmail.com>
2025-05-01 21:19:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function start(){
+      let img = document.getElementById('image')
+      img.src="bulb1.jpg"
+}
+function off(){
+       let img = document.getElementById('image')
+      img.src = "bulb.jpg"
+}
